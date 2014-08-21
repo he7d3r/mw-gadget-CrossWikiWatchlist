9494cecb7ecd18f0191c6969210f0ca543689f61
Helder <he7d3r@gmail.com>
2014-08-21 11:41:41 -0300

    Inform the license: CC BY-SA 3.0

--- xwikiwatchlist.js
+++ xwikiwatchlist.js
@@ -1,7 +1,8 @@
 /**
  * Cross-Wiki Watchlist
- * @author: [[User:Helder.wiki]]
- * @author: [[User:Legoktm]]
+ * @author: Helder (https://github.com/he7d3r)
+ * @author: Legoktm (https://github.com/legoktm)
+ * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/Cross-Wiki Watchlist.js]] ([[File:User:Helder.wiki/Tools/Cross-Wiki Watchlist.js]])
  */
 /*jshint browser:true, camelcase:true, curly:true, eqeqeq:true, immed:true, latedef:true,
